a8f10263108ed7056b6f8278b7688a9fb3301bee
shaoun11 <heyshaoun@gmail.com>
2023-10-19 18:49:23 +0600

    get-data-on-server

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ async function run() {
       res.send(result);
     });
 
+
+    
+
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
