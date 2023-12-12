309e267c3589877144aacd8c8a1d525f417b5fdd
shaoun-01 <heyshaoun@gmail.com>
2023-12-13 02:29:58 +0600

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const app=express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port=process.env.PORT||5000;
 
 
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}));
+app.use(express.json());
+app.use(cookieParser())
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzun1bo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
     // Send a ping to confirm a successful connection
     const database = client.db("Phone-data").collection("phones")
    
@@ -41,6 +47,19 @@ async function run() {
       console.log(result);
       res.send(result);
     });
+
+    //jw token
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+      res
+      .cookie("token",token,{
+        httpOnly:true,
+        secure:false,
+      
+      })
+      .send({Succcess:true});
+    })
 
     //get cart data
     app.get("/mycart", async (req, res) => {
@@ -95,10 +114,9 @@ app.put("/phones/:id", async (req, res) => {
       const result = await database.updateOne(filter,updatedata,options);
       res.send(result);
     });
+     
+ 
     
-
-    
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
@@ -108,8 +126,7 @@ run().catch(console.dir);
 
 
 
-app.use(cors());
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.send("Crud is running...");
